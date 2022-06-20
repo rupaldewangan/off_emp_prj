ad42a5a1be94e7df6b3f7920f7e2fb21a691a8df
RUPAL DEWANGAN <dewangan1501@gmail.com>
2022-06-21 00:22:14 +0530

    21.06.2022
    Django

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse
 from .models import department,Role,Employee
+from datetime import  datetime
 
 # Create your views here.
 def index(request):
@@ -11,7 +12,23 @@ def all_emp(request):
     return render(request,'all_emp.html',context)
 
 def add_emp(request):
-    return render(request,'add_emp.html')
+    if request.method == 'POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        salary=int(request.POST['salary'])
+        bonus=int(request.POST['bonus'])
+        phone=int(request.POST['phone'])
+        dept=int(request.POST['dept'])
+        role=int(request.POST['role'])
+        new_emp=Employee(first_name=first_name , last_name=last_name, salary=salary, bonus=bonus, phone = phone, dept_id= dept , role_id = role , hiredate= datetime.now())
+        new_emp.save()  
+        return HttpResponse("Data Added Sucessfully")
+    
+    elif request.method == 'GET':
+        return render(request,'add_emp.html')
+    
+    else:
+        return HttpResponse("Error occurred")
 
 def remove_emp(request):
     return render(request,'remove_emp.html')
